96eaccecef57775def66a291e77a6f97c288a5a8
Richard Kelters <r.kelters@flusso.nl>
2025-09-05 16:13:55 +0200

    28 opstart parameters met assemblies en preloadCLR

--- src/demo.sh
+++ src/demo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PROPATH=,/usr/dlc/tty/netlib/OpenEdge.Net.apl
+export PROPATH=../bin,.,/usr/dlc/tty/netlib/OpenEdge.Net.apl,../config
 
 rm -f out.log
 touch out.log
@@ -8,7 +8,7 @@ touch out.log
 # copy certificates to DLC folder, since -certstorepath can only contain 1 path
 cp -p certs/* /usr/dlc/certs
 
-# don't use -preloadCLR
-mbpro -clrnetcore -p flusso/demo/demo.p -param "$1,$2" > out.log
+mbpro -clrnetcore -assemblies ../assemblies -preloadCLR -q -rr -reusableObjects 500000 -p flusso/demo/demo.p -param "$1,$2" > out.log
 
 tail -f out.log
+
