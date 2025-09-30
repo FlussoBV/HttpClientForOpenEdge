df0ea8e62a4c07284bb9eb387caf21e1b4a07ea8
Arno van der Ende <a.vanderende@flusso.nl>
2025-09-30 09:24:32 +0200

    save caddy certificate for ABL httpclient

--- petstore-local/start-caddy.sh
+++ petstore-local/start-caddy.sh
@@ -3,6 +3,6 @@
 docker compose up -d
 
 # download the certificate
-docker exec caddy sh -c 'cat /data/caddy/pki/authorities/local/root.crt' > ../src/certs/caddy-root.crt
+docker exec caddy sh -c 'cat /data/caddy/pki/authorities/local/root.crt' > ../src/certs/84736cea.0
 
-ls -l ../src/certs/caddy-root.crt
+ls -l ../src/certs/84736cea.0
